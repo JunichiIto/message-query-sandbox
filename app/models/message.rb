9843e48d7bd2d2a1723f91762ff3ef6d0050b918
Junichi Ito <jit@sonicgarden.jp>
2015-03-15 07:18:12 +0900

    Add original implementation.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,28 @@
+module Messages
+  class HeadlinesFinder
+    def initialize(user)
+      @user = user
+      @messages = []
+      @other_user_ids = []
+    end
+
+    def call
+      messages = Message.order(created_at: :desc).related_to(@user)
+
+      messages.each do |message|
+        other_user_id = (message.from_user_id == @user.id) ? message.to_user_id : message.from_user_id
+
+        unless other_user_id.in?(@other_user_ids)
+          @messages << message
+          @other_user_ids << other_user_id
+        end
+      end
+
+      @messages
+    end
+  end
+end
+
 class Message < ActiveRecord::Base
   belongs_to :from_user, class_name: 'User', foreign_key: 'from_user_id'
   belongs_to :to_user, class_name: 'User', foreign_key: 'to_user_id'
@@ -5,4 +30,10 @@ class Message < ActiveRecord::Base
   validates :content, presence: true
   validates :from_user, presence: true
   validates :to_user, presence: true
+
+  scope :related_to, ->(user) { where('from_user_id = :user_id OR to_user_id = :user_id', user_id: user.id) }
+
+  def self.headlines_for(user)
+    Messages::HeadlinesFinder.new(user).call
+  end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -5,4 +5,30 @@ RSpec.describe Message, type: :model do
     expect(User.count).to be > 0
     expect(Message.count).to be > 0
   end
+
+  describe '::headlines_for' do
+    let(:contents) do
+      user = User.find_by_name user_name
+      messages = Message.headlines_for(user)
+      messages.map(&:content)
+    end
+    context 'はなこ' do
+      let(:user_name) { 'はなこ' }
+      it 'returns headlines' do
+        expect(contents).to eq %w(何だい、はなこ？ 何のようですか？たかし)
+      end
+    end
+    context 'たかし' do
+      let(:user_name) { 'たかし' }
+      it 'returns headlines' do
+        expect(contents).to eq %w(たかし、来週ひま？ 何のようですか？たかし)
+      end
+    end
+    context 'ひろし' do
+      let(:user_name) { 'ひろし' }
+      it 'returns headlines' do
+        expect(contents).to eq %w(何だい、はなこ？ たかし、来週ひま？)
+      end
+    end
+  end
 end
